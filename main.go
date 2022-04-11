32c7256d4a5bfff54fd4e01b093f201c092670a3
Alexander Anufriev <anufriev.alexander@gmail.com>
2022-04-11 21:06:24 +0300

    CLI mode + decline output to file

--- main.go
+++ main.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"github.com/aleksaan/diskusage/analyzer"
-	"github.com/aleksaan/diskusage/config"
-
-	"github.com/aleksaan/diskusage/console"
 	"github.com/aleksaan/diskusage/printer"
 )
 
@@ -14,11 +11,10 @@ func main() {
 	//var cfg = config.Cfg
 
 	//cfg.Load()
-	printer.Load()
-	printer.PrintAbout()
+	//printer.Load()
+	//printer.PrintAbout()
 	analyzer.Run()
 	printer.Run()
-	console.WaitExit(config.Cfg.Printer.ToTextFile == "")
-	printer.Close()
-
+	//console.WaitExit(config.Cfg.ToTextFile == "")
+	//printer.Close()
 }
